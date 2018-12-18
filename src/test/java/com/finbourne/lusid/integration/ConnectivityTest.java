bca79e802fa61ca0d6256119917a8ea8ca472e05
Rizwan Saeed <rizwan.saeed@finbourne.com>
2018-12-18 12:23:45 +0000

    Add proxy test

--- src/test/java/com/finbourne/lusid/integration/ConnectivityTest.java
+++ src/test/java/com/finbourne/lusid/integration/ConnectivityTest.java
@@ -1,13 +1,14 @@
 package com.finbourne.lusid.integration;
 
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
+import okhttp3.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -21,7 +22,7 @@ public class ConnectivityTest {
 
     @Parameterized.Parameters(name = "{index}: testUrl - {0}")
     public static Object[] data() {
-        return new Object[]{
+        return new Object[] {
                 "https://www.howsmyssl.com/a/check",
                 "https://api.lusid.com/api/metadata/versions",
                 "https://lusid.okta.com/oauth2/aus5al5yopbHW2wJn2p6/.well-known/oauth-authorization-server"
@@ -31,14 +32,45 @@ public class ConnectivityTest {
     @Test
     public void verify_connection() throws IOException {
 
-        OkHttpClient oktaClient = new OkHttpClient();
+        OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
                 .url(testUrl)
                 .header("Accept", "application/json")
                 .get()
                 .build();
 
-        Response response = oktaClient.newCall(request).execute();
+        Response response = client.newCall(request).execute();
+
+        assertThat(response.code(), is(equalTo(200)));
+    }
+
+    @Test
+    public void verify_connection_with_proxy() throws IOException {
+
+        //  this should be your proxy address
+        InetSocketAddress   proxy = new InetSocketAddress(InetAddress.getLocalHost(), 8888);
+
+        //  username and password for the proxy
+        String  proxyUsername = "user";
+        String  proxyPassword = "password";
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .proxy(new Proxy(Proxy.Type.HTTP, proxy))
+                .proxyAuthenticator((route, response) -> {
+                    String credential = Credentials.basic(proxyUsername, proxyPassword);
+                    return response.request().newBuilder()
+                            .header("Proxy-Authorization", credential)
+                            .build();
+                })
+                .build();
+
+        Request request = new Request.Builder()
+                .url(testUrl)
+                .header("Accept", "application/json")
+                .get()
+                .build();
+
+        Response response = client.newCall(request).execute();
 
         assertThat(response.code(), is(equalTo(200)));
     }
