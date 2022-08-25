55e7959cacf2e7e07c564a26cdb71fd1fe6c695a
Rizwan Saeed <rizwan.saeed@finbourne.com>
2022-08-25 16:41:06 +0100

    Update example and version of lusid sdk

--- src/test/java/com/finbourne/lusid/integration/LusidApiTests.java
+++ src/test/java/com/finbourne/lusid/integration/LusidApiTests.java
@@ -1,25 +1,35 @@
 package com.finbourne.lusid.integration;
 
 import com.finbourne.lusid.ApiClient;
-import com.finbourne.lusid.api.ScopesApi;
-import com.finbourne.lusid.model.ResourceListOfScopeDefinition;
+import com.finbourne.lusid.api.InstrumentsApi;
+import com.finbourne.lusid.model.InstrumentIdTypeDescriptor;
+import com.finbourne.lusid.model.ResourceListOfInstrumentIdTypeDescriptor;
 import com.finbourne.lusid.utilities.ApiClientBuilder;
+import com.finbourne.lusid.utilities.ApiConfiguration;
+import com.finbourne.lusid.utilities.ApiConfigurationBuilder;
 import org.junit.Test;
 
-import static org.junit.Assert.assertNotNull;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.notNullValue;
 
 public class LusidApiTests {
 
     @Test
     public void call_lusid_api() throws Exception {
 
-        ApiClient apiClient = new ApiClientBuilder().build("secrets.json");
-        ScopesApi scopesApi = new ScopesApi(apiClient);
+        ApiConfiguration apiConfiguration = new ApiConfigurationBuilder().build("secrets.json");
+        ApiClient apiClient = new ApiClientBuilder().build(apiConfiguration);
 
-        ResourceListOfScopeDefinition scopes = scopesApi.listScopes(null);
+        InstrumentsApi instrumentsApi = new InstrumentsApi(apiClient);
 
-        assertNotNull(scopes.getValues());
-        assertNotNull(scopes.getHref());
-        assertNotNull(scopes.getLinks());
+        ResourceListOfInstrumentIdTypeDescriptor instrumentIdentifierTypes = instrumentsApi.getInstrumentIdentifierTypes();
+
+        List<InstrumentIdTypeDescriptor> values = instrumentIdentifierTypes.getValues();
+
+        assertThat(values, notNullValue());
+        assertThat(values.size(), greaterThanOrEqualTo(0));
     }
 }
